44666fd13ffe5bf1428d94567aa6a0bf6c64c082
Yann Haefliger <yhaefliger@gmail.com>
2018-04-27 16:49:52 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,8 @@ gulp.task('purgecss', gulp.series(function(done){
             extractor: TailwindExtractor,
             extensions: ["erb"]
           }
-        ]
+        ],
+        whitelist: ['bg-bottom', 'bg-center', 'bg-left', 'bg-left-bottom', 'bg-left-top', 'bg-right', 'bg-right-bottom', 'bg-right-top', 'bg-top', 'a']
     })
   )
   .pipe(gulp.dest(dest+'css/'))
